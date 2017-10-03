bea58600925cb158f79d73a85083e812af70de8e
Eran Lunenfeld <lueran@gmail.com>
2017-10-03 20:41:36 +0300

    Added advance search

--- RIME/Controllers/BlogController.cs
+++ RIME/Controllers/BlogController.cs
@@ -65,13 +65,30 @@ namespace RIME.Controllers
         }
 
         // GET: Blog
-        public ActionResult Index(string query)
+        public ActionResult Index(string title, string author, string content, string date)
         {
             var evidences = from e in db.Evidences select e;
 
-            if (!String.IsNullOrEmpty(query))
+            if (!String.IsNullOrEmpty(title))
             {
-                evidences = evidences.Where(e => e.Title.Contains(query));
+                evidences = evidences.Where(e => e.Title.Contains(title));
+            }
+
+            if (!String.IsNullOrEmpty(author))
+            {
+                evidences = evidences.Where(e => e.UserName.Contains(author));
+            }
+
+            if (!String.IsNullOrEmpty(content))
+            {
+                evidences = evidences.Where(e => e.Content.Contains(content));
+            }
+
+            if (!String.IsNullOrEmpty(date))
+            {
+                var dt = Convert.ToDateTime(date);
+                evidences = evidences.Where(e => e.Date == dt);
+
             }
 
             return View(evidences.ToList());
